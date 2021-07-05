d44e3e0084ca505cb9816691bb9d84a6eff73e44
William Bohn <williamxavero@gmail.com>
2021-07-05 08:37:55 -0300

    validando campos - bean validation

--- src/main/kotlin/br/com/zup/autores/AutorRequest.kt
+++ src/main/kotlin/br/com/zup/autores/AutorRequest.kt
@@ -1,5 +1,11 @@
 package br.com.zup.autores
 
-data class AutorRequest(val nome: String,
-                        val email: String,
-                        val descricao: String)
+import io.micronaut.core.annotation.Introspected
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+@Introspected
+data class AutorRequest(@field:NotBlank val nome: String,
+                        @field:NotBlank @field:Email val email: String,
+                        @field:NotBlank @field:Size(max = 400) val descricao: String)
--- src/main/kotlin/br/com/zup/autores/CadastraAutoresController.kt
+++ src/main/kotlin/br/com/zup/autores/CadastraAutoresController.kt
@@ -3,12 +3,15 @@ package br.com.zup.autores
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
+@Validated
 @Controller("/autores")
 class CadastraAutoresController {
 
     @Post
-    fun cadastra(@Body novoAutor: AutorRequest){
+    fun cadastra(@Body @Valid novoAutor: AutorRequest){
         println(novoAutor.toString())
     }
 }
