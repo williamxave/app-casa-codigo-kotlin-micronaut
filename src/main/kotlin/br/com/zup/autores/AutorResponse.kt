5a7087b8928209604b037dd22a2ecfd54e5b6c06
William Bohn <williamxavero@gmail.com>
2021-07-05 16:56:22 -0300

    Retornando dados Json-Lista de autores

---
+++ src/main/kotlin/br/com/zup/autores/AutorResponse.kt
@@ -0,0 +1,9 @@
+package br.com.zup.autores
+
+class AutorResponse(autor: Autor){
+    val nome = autor.nome
+    val email = autor.email
+    val descricao = autor.descricao
+}
+
+
--- src/main/kotlin/br/com/zup/autores/CadastraAutoresController.kt
+++ src/main/kotlin/br/com/zup/autores/CadastraAutoresController.kt
@@ -1,7 +1,9 @@
 package br.com.zup.autores
 
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.validation.Validated
 import javax.validation.Valid
@@ -15,4 +17,21 @@ class CadastraAutoresController(val autorRepository: AutorRepository) {
     //val autor = novoAutor.paraAutor()
     //autorRepository.save(autor)
 
+    @Get
+    fun busca(): HttpResponse<List<AutorResponse>> {
+        val resposta = autorRepository.findAll()
+            .let { autores ->
+                autores.map { autor ->
+                    AutorResponse(autor)
+                }
+            }.run {
+                return HttpResponse.ok(this)
+            }
+    }
 }
+
+
+
+
+
+
