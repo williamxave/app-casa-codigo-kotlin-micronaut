e73cf74b73074dcea832644858762d41470f7338
William Bohn <williamxavero@gmail.com>
2021-07-07 12:13:27 -0300

    Serializando xml com micronaut

--- src/main/kotlin/br/com/zup/autores/CepClient.kt
+++ src/main/kotlin/br/com/zup/autores/CepClient.kt
@@ -1,6 +1,7 @@
 package br.com.zup.autores
 
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.client.annotation.Client
@@ -8,6 +9,7 @@ import io.micronaut.http.client.annotation.Client
 @Client(value = "\${url.api.cep}")
 interface CepClient {
 
-    @Get("/{cep}/json")
+    //@Get("/{cep}/json")
+    @Get(consumes = [ MediaType.APPLICATION_XML ],value = "/{cep}/xml") //  Para consumir xml
     fun buscaCep(@PathVariable cep: String) : HttpResponse<EnderecoResponse>
 }
